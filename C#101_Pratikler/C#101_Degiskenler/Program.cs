ab64474a7179437066d9220bcf3e7b7e088e758a
ELFUCAR <ucarelif.97@gmail.com>
2023-01-12 17:26:21 +0300

    project1

---
+++ C#101_Pratikler/C#101_Degiskenler/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C_101_Degiskenler;
+class Program
+{
+    static void Main(string[] args)
+    {
+           /* short s10=5;
+            ushort s11=5;
+
+            int i10=2;
+            Int16 i11=2;
+            Int32 i12=2;
+            Int64 i13=2;
+            uint i14=2;
+
+            float f10=5.4;
+            double d10=5.0;
+            long l10=5;
+
+             char ch='e';
+             string s10="Elif";
+
+             bool b10=10>4;
+             bool b11=10<4;*/
+
+             DateTime dt = DateTime.Now;
+             Console.WriteLine(dt);
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
