c42d7ff8063dff8efa71f47844dc37ab07b77ad4
ELFUCAR <ucarelif.97@gmail.com>
2023-01-24 12:39:05 +0300

    .

--- C#101_Konu_Pratikleri/koleksiyonlar_ArrayList/Program.cs
+++ C#101_Konu_Pratikleri/koleksiyonlar_ArrayList/Program.cs
@@ -1,8 +1,57 @@
-﻿namespace koleksiyonlar_ArrayList;
+﻿using System.Collections;
+
+namespace koleksiyonlar_ArrayList;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       //system.Collection namespace
+        ArrayList liste = new ArrayList();
+        liste.Add("Ayse");
+        liste.Add(2);
+        liste.Add(true);
+        liste.Add('A');
+
+        //icerisnde verilere erisim
+        Console.WriteLine(liste[1]);
+
+        foreach (var item in liste)
+            Console.WriteLine(item);
+        
+        //AddRange
+        Console.WriteLine("*** Add Range ***");
+       // string[] renkler ={"kirmizi","sari","yesil"};
+        List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+       // liste.AddRange(renkler);
+        liste.AddRange(sayilar);
+
+        foreach(var item in liste)
+            Console.WriteLine(item);
+
+        //Sort
+        Console.WriteLine("***Sort****");
+        liste.Sort();
+
+        foreach(var item in liste)
+            Console.WriteLine(item);
+
+        //Binary Search
+        Console.WriteLine("***Binary search**");
+        Console.WriteLine(liste.BinarySearch(9));
+
+        //reverse
+         Console.WriteLine("***Reverse**");
+         liste.Reverse();
+
+          foreach(var item in liste)
+            Console.WriteLine(item);
+
+        //Clear()  
+         Console.WriteLine("***Clear**");      
+        liste.Clear();
+
+         foreach(var item in liste)
+            Console.WriteLine(item);
+       
     }
 }
---
+++ C#101_Konu_Pratikleri/koleksiyonlar_dictionary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+namespace koleksiyonlar_dictionary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //System.Collection.Generic;
+        Dictionary<int,string> kullanicilar=new Dictionary<int, string>();
+
+        kullanicilar.Add(10,"Ayse Yilmaz");
+        kullanicilar.Add(12,"Ahmet Yildirim");
+        kullanicilar.Add(18,"Deniz Arda");
+        kullanicilar.Add(20,"Ozcan Cosar");
+     
+        //dizinin elemanlarına erisim
+        Console.WriteLine("***Elemanlara erisim");
+        Console.WriteLine(kullanicilar[12]);
+
+        foreach (var item in kullanicilar)
+            Console.WriteLine(item);
+
+        //Count
+        Console.WriteLine("***Count****");
+        Console.WriteLine(kullanicilar.Count);
+
+        //Contains
+        Console.WriteLine("***Contains***");
+        Console.WriteLine(kullanicilar.ContainsKey(12));
+        Console.WriteLine(kullanicilar.ContainsValue("Elif Ucar"));
+        
+        //Remove
+        Console.WriteLine("***Remove****");
+        kullanicilar.Remove(12);
+        foreach(var item in kullanicilar){
+            Console.WriteLine(item.Value);
+        } 
+
+        //Keys
+        Console.WriteLine("***Keys****");
+        foreach(var item in kullanicilar.Keys){
+            Console.WriteLine(item);
+        } 
+
+        //Values
+        Console.WriteLine("***Values****");
+        foreach(var item in kullanicilar.Values){
+            Console.WriteLine(item);
+        } 
+
+
+    }
+}
