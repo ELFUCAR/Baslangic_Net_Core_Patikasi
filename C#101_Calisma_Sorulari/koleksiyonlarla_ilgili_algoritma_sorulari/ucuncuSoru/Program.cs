783b041001080244d92c79372b0dc2ca6e9ea457
ELFUCAR <ucarelif.97@gmail.com>
2023-03-01 12:08:58 +0300

    gunc

--- C#101_Calisma_Sorulari/koleksiyonlarla_ilgili_algoritma_sorulari/ucuncuSoru/Program.cs
+++ C#101_Calisma_Sorulari/koleksiyonlarla_ilgili_algoritma_sorulari/ucuncuSoru/Program.cs
@@ -3,6 +3,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+            char[] arr= {'a','A','e','E','i','İ','I','o','O','Ö','ö','u','Ü'};
+
+            string sentence=Console.ReadLine();
+            char[] sentChar; 
+
+
+            foreach(char c in sentence.ToCharArray()){
+                for(int i=0; i<sentence.Length; i++)
+                     if(c==arr[i])
+                        sentChar[i]=c;
+            }
+
+            
+        Console.WriteLine('Hello, World!');
     }
 }
